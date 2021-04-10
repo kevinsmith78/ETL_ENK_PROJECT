aacb1152e42320b2577c97f648852fc769642a65
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-10 11:55:46 -0500

    adding

--- happiness_population.sql
+++ happiness_population.sql
@@ -3,18 +3,17 @@ CREATE TABLE "happiness" (
     "country_name" VARCHAR(30)   NOT NULL,
     "score" numeric NOT NULL
 );
-
+​
 CREATE TABLE "population" (
     "country_name" VARCHAR(100)   NOT NULL,
     "2019_population" numeric NULL
 );
-
-select * from happiness
-
-select * from population
-
+​
+CREATE TABLE happiness_population AS
 select happiness.country_name, happiness.overall_rank, round(happiness.score,2) as score, population."2019_population"
 from happiness
 left join population
 on population.country_name = happiness.country_name
 order by happiness.overall_rank
+​
+SELECT * FROM happiness_population
